281b532c08a497fd6d77258d3d57fa3eb1742d39
Anantha Debnath <ananthadebnath103@gmail.com>
2023-07-31 03:06:59 +0600

    admin check done

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react"
 
 const useAdmin = email => {
-    const [isAdmin, setIsAdmin] = useState(false);
+    const [Admin, setAdmin] = useState(false);
     const [adminLoading, setAdminLoading] = useState(true)
     useEffect(() => {
         setAdminLoading(true)
         if (email) {
-            fetch(`https://assignment-12-sarver.vercel.app/users/admin/${email}`)
+            fetch(`https://assignment-12-sarver.vercel.app/users/${email}`)
                 .then(res => res.json())
                 .then(data => {
                     if (data.isAdmin) {
-                        setIsAdmin(data.isAdmin)
+                        setAdmin(data.isAdmin)
                         setAdminLoading(false)
                     }
                 })
         }
     }, [email])
-    return [isAdmin, adminLoading]
+    return [Admin, adminLoading]
 }
 
 export default useAdmin;
